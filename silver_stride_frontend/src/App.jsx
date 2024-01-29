41eabed84830b5cc27f0b768ff22050a17f54de5
Bhaavesh Waykole <bhaaveshw@gmail.com>
2024-01-29 09:22:00 +0530

    tailwind config done

--- silver_stride_frontend/src/App.jsx
+++ silver_stride_frontend/src/App.jsx
@@ -1,28 +1,8 @@
-import React, { useState } from 'react';
-import './App.css';
-
+// import './App.css';
 function App() {
-  const [generatedNumber, setGeneratedNumber] = useState(null);
-
-  const generateNumber = async () => {
-    try {
-      const response = await fetch('http://localhost:3001/generate-number');
-      const data = await response.json();
-      setGeneratedNumber(data.number);
-    } catch (error) {
-      console.error('Error fetching number:', error);
-    }
-  };
-
   return (
-    <div className="App">
-      <header className="App-header">
-        <h1>Silver Stride AI - Number Generation App</h1>
-        <button onClick={generateNumber}>Generate Number</button>
-        {generatedNumber !== null && (
-          <p>Generated Number: {generatedNumber}</p>
-        )}
-      </header>
+    <div>
+      <h1 className="bg-orange-100">HELLO</h1>
     </div>
   );
 }
