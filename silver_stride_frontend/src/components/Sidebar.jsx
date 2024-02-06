af9bcf6a05a87b9793f919af75119f901f285c6d
Bhaavesh Waykole <bhaaveshw@gmail.com>
2024-02-07 00:00:37 +0530

    login, signup pg

--- silver_stride_frontend/src/components/Sidebar.jsx
+++ silver_stride_frontend/src/components/Sidebar.jsx
@@ -30,17 +30,17 @@ export function Sidebar() {
             </div>
             <List>
                 <div className="px-2 flex content-center flex-col self-center">
-                    <ListItem className="gap-3 mb-2 items-center cursor-pointer">
+                    <ListItem className="gap-3 mb-2 items-center cursor-pointer w-full rouned-xl">
                         <ListItemPrefix>
                             <ChartBarSquareIcon className="h-5 w-5" />
                         </ListItemPrefix>
-                        DashBord
+                        <span>DashBoard</span>
                     </ListItem>
                     <ListItem className="gap-3 mb-2 items-center cursor-pointer">
                         <ListItemPrefix>
                             <ArrowTrendingUpIcon className="h-5 w-5" />
                         </ListItemPrefix>
-                        Learn
+                        <span>Learn</span>
                     </ListItem>
                     <ListItem className="gap-3 mb-2 items-center cursor-pointer">
                         <ListItemPrefix>
